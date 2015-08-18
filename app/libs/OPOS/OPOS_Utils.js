e46d6c5f78a647958ab49dffcf4a45563c00fe42
StipJey <StipJey@gmail.com>
2015-08-18 12:11:51 +0300

    Доработка PrintBundle

--- app/libs/OPOS/OPOS_Utils.js
+++ app/libs/OPOS/OPOS_Utils.js
@@ -54,54 +54,58 @@ define(function (require) {
         };
 
         self.printBundle = function(aOptions, aFirstValue, aSecondValue, aFont){
-            aFirstValue = aFirstValue.toString();
-            aSecondValue = aSecondValue.toString();
-            if (!aFont) aFont = 0;
             var data = [];
-            var newLine = 0;
-            var spaces = 0;
-            switch (aFont){
-                case 0: case 16: case 128: case 144:
-                    if (aFirstValue.length + aSecondValue.length > aOptions.first){
-                        newLine = 1; break;
-                    }
-                    spaces = aOptions.first - (aFirstValue.length + aSecondValue.length);
-                    break;
-                case 32: case 48: case 160: case 176:
-                    if (aFirstValue.length + aSecondValue.length > aOptions.firstDouble){
-                        newLine = 1; break;
-                    }
-                    spaces = aOptions.firstDouble - (aFirstValue.length + aSecondValue.length);
-                    break;
-                case 1: case 17:
-                    if (aFirstValue.length + aSecondValue.length > aOptions.second){
-                        newLine = 1; break;
-                    }
-                    spaces = aOptions.second - (aFirstValue.length + aSecondValue.length);
-                    break;
-                case 33: case 49:
-                    if (aFirstValue.length + aSecondValue.length > aOptions.secondDouble){
-                        newLine = 1; break;
-                    }
-                    spaces = aOptions.secondDouble - (aFirstValue.length + aSecondValue.length);
-                    break;
-                default :
-                    newLine = 0;
-                    spaces = 0;
-            }
-
-            data = data.concat([27, 69, 48, 27, 45, 48, 27, 33, aFont ? aFont : 0, 29, 66, 48, 27, 97, 48, 27, 51, 60, 27, 71, 49]);
-
-            if (newLine){
-                data = data.concat(self.printLine(aFirstValue, "left", aFont));
-                data = data.concat(self.printLine(aSecondValue, "right", aFont));
-            } else {
-                data = data.concat(stringToBytes(aFirstValue));
-                for (var counter = 0; counter < spaces; counter++){
-                    data.push(32);
+            aFirstValue = aFirstValue.toString();
+            if (!aSecondValue) 
+                data = data.concat(self.printLine(aFirstValue, "center", aFont));
+            else {
+                aSecondValue = aSecondValue.toString();
+                if (!aFont) aFont = 0;
+                var newLine = 0;
+                var spaces = 0;
+                switch (aFont){
+                    case 0: case 16: case 128: case 144:
+                        if (aFirstValue.length + aSecondValue.length > aOptions.first){
+                            newLine = 1; break;
+                        }
+                        spaces = aOptions.first - (aFirstValue.length + aSecondValue.length);
+                        break;
+                    case 32: case 48: case 160: case 176:
+                        if (aFirstValue.length + aSecondValue.length > aOptions.firstDouble){
+                            newLine = 1; break;
+                        }
+                        spaces = aOptions.firstDouble - (aFirstValue.length + aSecondValue.length);
+                        break;
+                    case 1: case 17:
+                        if (aFirstValue.length + aSecondValue.length > aOptions.second){
+                            newLine = 1; break;
+                        }
+                        spaces = aOptions.second - (aFirstValue.length + aSecondValue.length);
+                        break;
+                    case 33: case 49:
+                        if (aFirstValue.length + aSecondValue.length > aOptions.secondDouble){
+                            newLine = 1; break;
+                        }
+                        spaces = aOptions.secondDouble - (aFirstValue.length + aSecondValue.length);
+                        break;
+                    default :
+                        newLine = 0;
+                        spaces = 0;
                 }
-                data = data.concat(stringToBytes(aSecondValue));
-                data.push(10);
+    
+                data = data.concat([27, 69, 48, 27, 45, 48, 27, 33, aFont ? aFont : 0, 29, 66, 48, 27, 97, 48, 27, 51, 60, 27, 71, 49]);
+    
+                if (newLine){
+                    data = data.concat(self.printLine(aFirstValue, "left", aFont));
+                    data = data.concat(self.printLine(aSecondValue, "right", aFont));
+                } else {
+                    data = data.concat(stringToBytes(aFirstValue));
+                    for (var counter = 0; counter < spaces; counter++){
+                        data.push(32);
+                    }
+                    data = data.concat(stringToBytes(aSecondValue));
+                    data.push(10);
+                }
             }
 
             return data;
