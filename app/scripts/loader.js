38e190789d1aa3af9e6bfeb5bf849679f56cfb1d
Mikhail Lapshin <lapshin.michail@gmail.com>
2015-12-08 18:17:46 +0300

    запуск приложения в fullscreen режиме

--- app/scripts/loader.js
+++ app/scripts/loader.js
@@ -3,6 +3,7 @@ require(['AppAPI', 'webview', 'DeviceHandler', 'apiEvents'], function(AppAPI, we
             setTimeout(function() {
                 //AppAPI(JSON.stringify({login:"pzapadmilk", pass:"barista"}), 'autologin');
                 console.log("load ok!");
+				chrome.app.window.get("4RPOS").fullscreen();
             }, 1000);
         });
     });
--- app/scripts/webview.js
+++ app/scripts/webview.js
@@ -2,7 +2,8 @@ define(function() {
     var container = document.getElementById('body');
     var webviewElement = document.createElement('webview');
     webviewElement.id = 'webview';
-    webviewElement.src = 'http://localhost:8080/erpCafe/application-start.html';
+    //webviewElement.src = 'http://rccoffee.4rp.org:8080/cafeErp/application-start.html';
+    webviewElement.src = 'http://localhost:8084/erpCafe/application-start.html';
     //webviewElement.src = 'http://localhost:63342/app/test.html';
     //webviewElement.src = 'http://localhost:63342/app/test_scale.html';
     container.appendChild(webviewElement);
